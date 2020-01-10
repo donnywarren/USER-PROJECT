06562a59853d58951fc3247d0692a2bf0847aa8f
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-09 20:08:04 -0500

    set up

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//necessary for the page to load before the scripts run
+//probably will need every time
+document.addEventListener("DOMContentLoaded", function(){
+
+
+//all other code goes in here
+
+
+//select the form
+var form = document.querySelector("form");
+
+
+//add an event listeners on form submit
+form.addEventListener("submit", function(event) {
+
+  //prevent the default functionality of htis FORM
+  event.preventDefault();
+
+  var firstNameInput = document.querySelector(".first-name");
+
+  var lastNameInput = document.querySelector(".last-name");
+
+  var firstNameValue = firstNameInput.value;
+
+  var lastNameValue = lastNameInput.value;
+  console.log(firstNameValue, lastNameValue);
+
+
+});
+
+
+
+
+
+
+
+});
